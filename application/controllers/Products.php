729994abc2af79890690e37589b3b3364007b38b
Aynev <seon310@gmail.com>
2018-10-08 13:42:41 +0900

    댓글 정보 가져오기 구현

--- application/controllers/Products.php
+++ application/controllers/Products.php
@@ -23,10 +23,42 @@ class Products extends CI_Controller
         $product_data['keywords'] = $this->get_keywords($product_id);
         $product_data['images'] = $this->get_product_images($product_id);
         $assessment_page = intval($this->input->get('a_page'));
-
         $product_data['assessments'] = $this->get_assessments($product_id, $assessment_page);
+        $comment_page = intval($this->input->get('c_page'));
+        $product_data['comments'] = $this->get_comments($product_id, $comment_page);
 
         echo json_encode($product_data, JSON_UNESCAPED_UNICODE);
+    }
+
+    private function get_comments($product_id, $page)
+    {
+        $limit = 10;
+        $offset = $page ? ($page - 1) * $limit : 0;
+
+        $sql = "SELECT
+                  parent.id, parent.created_at, parent.name, parent.user_portrait, parent.comment,
+                  child.reply
+                FROM
+                  (SELECT
+                    parent.id, parent.user_id, parent.parent_id, parent.comment, parent.created_at,
+                    u.name, u.portrait_url AS user_portrait
+                  FROM
+                    product_comments AS parent
+                  INNER JOIN
+                    users AS u
+                  ON
+                    parent.user_id = u.id AND parent_id is null AND parent.product_id = ?
+                  LIMIT ? OFFSET ?) as parent
+                LEFT JOIN
+                  (SELECT parent_id, comment AS reply FROM product_comments WHERE parent_id is not null) AS child
+                ON
+                  parent.id = child.parent_id
+                ORDER BY parent.id DESC";
+        $query = $this->db->query($sql, [$product_id, $limit, $offset]);
+        $result['list'] = $query->result();
+        $result['page'] = $page ? $page : 1;
+
+        return $result;
     }
 
     private function serialized($values)
